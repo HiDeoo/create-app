f06a5ff8e020ba7104e7ce167d221e1f86a9ca59
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-08-04 15:19:28 +0200

    build: add Prettier

--- build.config.ts
+++ build.config.ts
@@ -1,7 +1,7 @@
-import { defineBuildConfig } from "unbuild";
+import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   clean: true,
   declaration: false,
-  entries: ["src/cli"],
-});
+  entries: ['src/cli'],
+})
